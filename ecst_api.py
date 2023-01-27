cf4a30d128296de13106b78ecec14b4cd942325b
sotoq-sergio <sotoq.sergio@gmail.com>
2023-01-26 21:27:43 -0600

    Removing unused packages

--- ecst_api.py
+++ ecst_api.py
@@ -1,12 +1,8 @@
 # Import packages and files
-import os 
 import time
 from datetime import datetime
 from selenium import webdriver
-from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from private.credentials import username, password
 from private.homeroom_roster import homeroom_roster
 
